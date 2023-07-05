735bc5ffca59ac84127566860ff36cb062c4e102
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-07-05 13:01:17 -0300

    Priojeto em desenvolvimento

--- src/app/Search/page.tsx
+++ src/app/Search/page.tsx
@@ -1,38 +1,72 @@
 'use client'
 import React, {useState, useEffect} from 'react'
 import {MovieCards} from "../components/MovieCards";
-import { useRouter } from 'next/navigation'
+import { useRouter, usePathname  } from 'next/navigation'
+import { NextResponse } from 'next/server'
 
-type Props = {
-  search: string
+interface Movie {
+  title: string;
+  // outras propriedades do filme
 }
 
-export default function Search({ search }:Props) {
-      const [movies, setMovies] = useState([]);
-      const router = useRouter()
+export default function Search() {
+      const [movies, setMovies] = useState<Movie[]>([]);
+      const [url, seturl] = useState<string | null>('');
+      const [showLink, setShowLink] = useState(true);
 
 
+      useEffect(() => {
+        const url = new URL(window.location.href);
+        const query = url.searchParams.get('q');
+        console.log(query);
+
+        seturl(query);
+        
+      },)
+
+      useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/search/movie?query=${url}&api_key=506fadb0256c13349acc05dabebf9604`, {
+            method: 'GET',
+           })
+           .then(response => response.json())
+           .then((json) => {
+            setMovies(json.results);
+           })      
+    }, [url])
+      
+    console.log(movies);
+    
     return(
       <div className=' text-[#fff]  m-[2rem] '>
-      <h2 className='text-[2.5rem] text-center'>Resultados para:  {search}
+        <h2 className='text-[2.5rem] text-center'
+        >Resultados para: <span
+                            className='text-[#f7d354]'
+                           >
+                            {url}
+                            </span>
       </h2>
 
       <div className='flex flex-wrap	justify-between'>
-     {
-       movies.length === 0 ? (
-         <div>Carregando...</div>
-       ) : (
-         movies.map((movie, index) => (
-           <div key={index} 
-             className='flex  flex-wrap
-                         justify-between md:w-[30%]	'
-           >
-            
-           </div>
-         ))
-       )
-     }
-      </div>
+      {
+        movies.length === 0 ? (
+          <div>Carregando...</div>
+        ) : (
+          movies.map((movie) => (
+            <div key={movie.title} 
+              className='flex  flex-wrap
+                          justify-between md:w-[30%]	'
+            >
+                <MovieCards
+                  movie={movie}
+                  showLink={showLink}
+                  setShowLink={setShowLink}
+                  />
+
+            </div>
+          ))
+        )
+      }
+       </div>
    </div>
     )
   }
--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,3 +1,53 @@
-export default function Movie() {
-    return <h1>Eu estou na página movie</h1>
+'use client'
+import React, {useState, useEffect} from 'react'
+import {MovieCards} from "../components/MovieCards";
+import { useRouter, usePathname  } from 'next/navigation'
+
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
+type Props = {
+
+}
+import { NextResponse } from 'next/server'
+
+
+export default function Movie(request: Request) {
+  const [movies, setMovies] = useState<null | Props[]>(null);
+  const [url, seturl] = useState<string | null>('');
+
+useEffect(() => {
+  const teste = async () => {
+    const { searchParams } = new URL(request.url)
+    const id = searchParams.get('id')
+    seturl(id);
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${url}?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=1`)
+    const product = await res.json()
+   
+    setMovies(product)
+  }
+      teste();
+}, [])
+
+
+    
+/*   useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/39108?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=1`, {
+        method: 'GET',
+       })
+       .then(response => response.json())
+       .then((json) => {
+        setMovies(json);
+       })      
+}, []) */
+
+    return (
+      <div>
+       <p>Paranetro:{url}</p>
+      </div>
+    )
   }
